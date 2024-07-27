441652d404092a353c1f81dbb0969a11fcf948c6
sejal-jain123 <115560081+sejal-jain123@users.noreply.github.com>
2024-07-28 00:00:57 +0530

    RoboCar_code

---
+++ voice_controlled_car_code/voice_controlled_car_code.ino
@@ -0,0 +1,71 @@
+#define m1 2
+#define m2 3
+#define m3 4
+#define m4 5
+
+void setup()
+{
+pinMode(m1, OUTPUT);
+pinMode(m2, OUTPUT);
+pinMode(m3, OUTPUT);
+pinMode(m4, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+if (Serial.available())
+{
+String voice = Serial.readString();
+Serial.println(voice);
+if(voice == "forward")
+{
+digitalWrite(m1, HIGH);
+digitalWrite(m2, LOW);
+digitalWrite(m3, HIGH);
+digitalWrite(m4, LOW);
+}
+if(voice == "backward")
+{
+digitalWrite(m2, HIGH);
+digitalWrite(m1, LOW);
+digitalWrite(m4, HIGH);
+digitalWrite(m3, LOW);
+}
+if(voice == "left")
+{
+digitalWrite(m1, LOW);
+digitalWrite(m2, LOW);
+digitalWrite(m3, HIGH);
+digitalWrite(m4, LOW);
+}
+if(voice == "right")
+{
+digitalWrite(m1, HIGH);
+digitalWrite(m2, LOW);
+digitalWrite(m3, LOW);
+digitalWrite(m4, LOW);
+}
+else if(voice == "stop")
+{
+digitalWrite(m1, LOW);
+digitalWrite(m2, LOW);
+digitalWrite(m3, LOW);
+digitalWrite(m4, LOW);
+}
+}
+}
+
+
+
+
+
+// void setup() {
+//   // put your setup code here, to run once:
+
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+
+// }
